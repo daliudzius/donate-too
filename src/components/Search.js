eca85f912863717af5c4349de98bae5c2d9806e0
daliudzius <daliudzius@gmail.com>
2022-02-08 04:14:52 -0800

    Add full search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,60 +1,90 @@
 /**
- * Renders and handles the search functionality.
+ * Handles the search functionality search rendering.
  *
  * Handles the search input, and passes the search results to the
  * SearchResults component.
  */
 
-import {
-   Button,
-   Input,
-   useBoolean,
-   FormControl,
-   VisuallyHidden,
-} from '@chakra-ui/react'
+import { Input } from '@chakra-ui/react'
 import SearchResults from '../components/SearchResults'
 import NpoResults from '../components/NpoResults'
-import { supabase } from '../utils/supabaseClient'
 import { useState } from 'react'
 import Fuse from 'fuse.js'
 
-const handleSubmit = () => {
-   setIsSearch.toggle
-}
+const MAX_RESULTS = 4
 
 const Search = ({ items, npos }) => {
-   const [isSearch, setIsSearch] = useBoolean(true)
-   const [searchQuery, setSearchQuery] = useState("");
-   const [searchResults, setSearchResults] = useState([]);
+   const [searchInput, setSearchInput] = useState('')
+   const [searchResults, setSearchResults] = useState([])
+   const [selectedIndex, setSelectedIndex] = useState(0)
+   const [selectedItem, setSelectedItem] = useState({})
 
+   // initialize the fuse search options
    const fuse = new Fuse(items, {
       includeScore: true,
-      threshold: 0.4,
-      keys: ['name'],
+      threshold: 0.5,
+      keys: [{ name: 'name', weight: 10 }, 'category'],
    })
 
-   const handleSearch = (searchQuery) => {
-      setSearchQuery(searchQuery);
-      const results = fuse.search(searchQuery);
-      setSearchResults(results);
-    };
+   // handles keypresses for Enter, Up, Down
+   const handleKeyDown = (pressedKey) => {
+      switch (pressedKey) {
+         case 'Enter':
+            // if 'Show All Categories' is selected
+            if (selectedIndex === searchResults.length) {
+               alert('To-do: Show the categories.')
+            }
+            // if an item is selected
+            if (searchResults.length > 0) {
+               let currentItem = searchResults[selectedIndex].item
+               setSelectedItem(currentItem)
+               setSearchInput(currentItem.name)
+            }
+            setSelectedIndex(0)
+            break
+         case 'ArrowUp':
+            selectedIndex > 0 && setSelectedIndex((prev) => prev - 1)
+            break
+         case 'ArrowDown':
+            // +1 to account for 'Show All Categories option
+            selectedIndex < searchResults.length &&
+               setSelectedIndex((prev) => prev + 1)
+            break
+      }
+   }
+
+   // handles search and sets the search results array
+   const handleSearch = (query) => {
+      // update input value
+      setSearchInput(query)
+
+      // update fuzzy search results
+      const results = fuse.search(query).slice(0, MAX_RESULTS)
+      setSearchResults(results)
+
+      // reset selected index on input update
+      setSelectedIndex(0)
+   }
 
    return (
       <>
-         <form onSubmit={setIsSearch.on}>
-            <FormControl>
-               <Input
-                  placeholder={isSearch ? 'i can donate...' : 'Item Searched'}
-                  variant={'filled'}
-                  size={'lg'}
-                  px={8}
-                  value={searchQuery}
-                  onChange={(event) => handleSearch(event.target.value)}
-               />
-            </FormControl>
-         </form>
-         <SearchResults items={searchResults} />
-         {!isSearch && <NpoResults npos={npos} />}
+         <Input
+            placeholder={selectedItem ? 'i can donate...' : 'Item Searched'}
+            variant={'filled'}
+            fontSize={'2xl'}
+            px={8}
+            value={searchInput}
+            onChange={(event) => handleSearch(event.target.value)}
+            onKeyDown={(event) => handleKeyDown(event.key)}
+            h={16}
+         />
+         {selectedItem && (
+            <SearchResults
+               items={searchResults}
+               selectedIndex={selectedIndex}
+            />
+         )}
+         {!selectedItem && <NpoResults npos={npos} />}
       </>
    )
 }
